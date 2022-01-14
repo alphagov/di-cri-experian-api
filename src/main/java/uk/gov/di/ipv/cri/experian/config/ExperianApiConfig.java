800b90c166fab97f99653bd1bb87e333eb504417
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2022-01-14 17:05:48 +0000

    Deploy lambda to aws

--- src/main/java/uk/gov/di/ipv/cri/experian/config/ExperianApiConfig.java
+++ src/main/java/uk/gov/di/ipv/cri/experian/config/ExperianApiConfig.java
@@ -1,14 +1,19 @@
 package uk.gov.di.ipv.cri.experian.config;
 
+import software.amazon.lambda.powertools.parameters.SSMProvider;
+import software.amazon.lambda.powertools.parameters.SecretsProvider;
+
 public class ExperianApiConfig {
     private final String tenantId;
     private final String endpointUri;
     private final String hmacKey;
 
-    public ExperianApiConfig() {
-        this.tenantId = System.getenv("EXPERIAN_API_TENANT_ID");
-        this.endpointUri = System.getenv("EXPERIAN_API_ENDPOINT_URI");
-        this.hmacKey = System.getenv("EXPERIAN_API_HMAC_KEY");
+    public ExperianApiConfig(SSMProvider ssmProvider, SecretsProvider secretsProvider) {
+        this.tenantId = ssmProvider.get("/dev/di-cri-experian-fraud-api/experian-api-tenant-id");
+
+        this.endpointUri = ssmProvider.get("/dev/di-cri-experian-fraud-api/experian-api-endpoint");
+
+        this.hmacKey = secretsProvider.get("/dev/di-cri-experian-fraud-api/experian-api-hmac-key");
     }
 
     public String getTenantId() {
--- src/main/java/uk/gov/di/ipv/cri/experian/handler/IdentityCheckHandler.java
+++ src/main/java/uk/gov/di/ipv/cri/experian/handler/IdentityCheckHandler.java
@@ -8,7 +8,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.lambda.powertools.parameters.ParamManager;
+import software.amazon.lambda.powertools.parameters.SecretsProvider;
 import uk.gov.di.ipv.cri.experian.config.ExperianApiConfig;
+import uk.gov.di.ipv.cri.experian.domain.PersonIdentity;
+import uk.gov.di.ipv.cri.experian.domain.ValidationResult;
 import uk.gov.di.ipv.cri.experian.gateway.ExperianApiRequestMapper;
 import uk.gov.di.ipv.cri.experian.gateway.ExperianGateway;
 import uk.gov.di.ipv.cri.experian.gateway.HmacGenerator;
@@ -21,9 +25,12 @@ import javax.validation.ValidatorFactory;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Map;
 
 public class IdentityCheckHandler
@@ -33,28 +40,31 @@ public class IdentityCheckHandler
     private static final int CREATED_HTTP_STATUS_CODE = 201;
     private static final int INTERNAL_SERVER_ERROR_STATUS_CODE = 500;
     private static final Logger LOGGER = LoggerFactory.getLogger(IdentityCheckHandler.class);
+    private static final SecretsProvider SECRETS_PROVIDER = ParamManager.getSecretsProvider();
 
     private final IdentityVerificationService identityVerificationService;
     private final ObjectMapper objectMapper;
     private final InputValidationExecutor inputValidationExecutor;
 
     static {
-        //        try {
-        //            String keystoreBase64 = System.getenv("KEYSTORE");
-        //            Path tempFile = Files.createTempFile(null, null);
-        //            Files.write(tempFile, Base64.getDecoder().decode(keystoreBase64));
-        //
-        //            System.setProperty("javax.net.ssl.keyStore", tempFile.toString());
-        //            System.setProperty("javax.net.ssl.keyStoreType", "pkcs12");
-        //            System.setProperty(
-        //                    "javax.net.ssl.keyStorePassword", System.getenv("KEYSTORE_PASSWORD"));
-        //        } catch (IOException e) {
-        //            LOGGER.error("Static initialisation failed", e);
-        //        }
+        try {
+            String keystoreBase64 =
+                    SECRETS_PROVIDER.get("/dev/di-cri-experian-fraud-api/experian-api");
+            Path tempFile = Files.createTempFile(null, null);
+            Files.write(tempFile, Base64.getDecoder().decode(keystoreBase64));
+
+            System.setProperty("javax.net.ssl.keyStore", tempFile.toString());
+            System.setProperty("javax.net.ssl.keyStoreType", "pkcs12");
+            System.setProperty(
+                    "javax.net.ssl.keyStorePassword",
+                    SECRETS_PROVIDER.get(
+                            "/dev/di-cri-experian-fraud-api/experian-api-keystore-password"));
+        } catch (IOException e) {
+            LOGGER.error("Static initialisation failed", e);
+        }
     }
 
-    public IdentityCheckHandler()
-            throws NoSuchAlgorithmException, InvalidKeyException, IOException {
+    public IdentityCheckHandler() throws NoSuchAlgorithmException, InvalidKeyException {
         this.objectMapper = new ObjectMapper();
         this.objectMapper.registerModule(new JavaTimeModule());
 
@@ -67,8 +77,7 @@ public class IdentityCheckHandler
     public IdentityCheckHandler(
             IdentityVerificationService identityVerificationService,
             ObjectMapper objectMapper,
-            InputValidationExecutor inputValidationExecutor)
-            throws IOException {
+            InputValidationExecutor inputValidationExecutor) {
         this.identityVerificationService = identityVerificationService;
         this.objectMapper = objectMapper;
         this.inputValidationExecutor = inputValidationExecutor;
@@ -78,39 +87,36 @@ public class IdentityCheckHandler
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
         Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
-        return createResponseEvent(201, "{\"field\": \"value\"}", responseHeaders);
-        //
-        //
-        //        try {
-        //            PersonIdentity personIdentity =
-        //                    objectMapper.readValue(input.getBody(), PersonIdentity.class);
-        //            ValidationResult validationResult =
-        //                    this.inputValidationExecutor.performInputValidation(personIdentity);
-        //
-        //            String responseBody;
-        //            int responseStatusCode;
-        //
-        //            if (validationResult.isValid()) {
-        //                responseStatusCode = CREATED_HTTP_STATUS_CODE;
-        //                responseBody =
-        // this.identityVerificationService.verifyIdentity(personIdentity);
-        //            } else {
-        //                responseStatusCode = BAD_REQUEST_HTTP_STATUS_CODE;
-        //                responseBody = objectMapper.writeValueAsString(validationResult);
-        //            }
-        //
-        //            return createResponseEvent(responseStatusCode, responseBody, responseHeaders);
-        //        } catch (Exception e) {
-        //            LOGGER.error("Error handling request", e);
-        //            return createResponseEvent(
-        //                    INTERNAL_SERVER_ERROR_STATUS_CODE, "An error occurred",
-        // responseHeaders);
-        //        }
+
+        try {
+            PersonIdentity personIdentity =
+                    objectMapper.readValue(input.getBody(), PersonIdentity.class);
+            ValidationResult validationResult =
+                    this.inputValidationExecutor.performInputValidation(personIdentity);
+
+            String responseBody;
+            int responseStatusCode;
+
+            if (validationResult.isValid()) {
+                responseStatusCode = CREATED_HTTP_STATUS_CODE;
+                responseBody = this.identityVerificationService.verifyIdentity(personIdentity);
+            } else {
+                responseStatusCode = BAD_REQUEST_HTTP_STATUS_CODE;
+                responseBody = objectMapper.writeValueAsString(validationResult);
+            }
+
+            return createResponseEvent(responseStatusCode, responseBody, responseHeaders);
+        } catch (Exception e) {
+            LOGGER.error("Error handling request", e);
+            return createResponseEvent(
+                    INTERNAL_SERVER_ERROR_STATUS_CODE, "An error occurred", responseHeaders);
+        }
     }
 
     private IdentityVerificationService createIdentityVerificationService(ObjectMapper objectMapper)
             throws NoSuchAlgorithmException, InvalidKeyException {
-        ExperianApiConfig experianExperianApiConfig = new ExperianApiConfig();
+        ExperianApiConfig experianExperianApiConfig =
+                new ExperianApiConfig(ParamManager.getSsmProvider(), SECRETS_PROVIDER);
         HttpClient httpClient =
                 HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(20)).build();
         HmacGenerator hmacGenerator = new HmacGenerator(experianExperianApiConfig.getHmacKey());
